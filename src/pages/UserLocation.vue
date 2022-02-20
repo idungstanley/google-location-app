08bdbe34b532146bee4c38861808bdfb3c36bf55
sunday stanley Idung <sundaystanley56@gmail.com>
2022-02-20 19:50:05 +0100

    added styles

---
+++ src/pages/UserLocation.vue
@@ -0,0 +1,25 @@
+<template>
+ <section class="ui two column centered grid">
+   <div class="column">
+     <form action="" class="ui segment large form">
+       <div class="ui message red"></div>
+       <div class="ui segment">
+         <div class="field">
+           <div class="ui right icon input large">
+             <input type="text" placeholder="Enter your address">
+             <i class="circle icon"></i>
+           </div>
+         </div>
+         <button class="ui button" @click="locatorButtonPressed">Go</button>
+       </div>
+     </form>
+   </div>
+ </section>
+</template>
+
+<style>
+.ui.button, .dot.circle.icon{
+  background-color: #ff5a5f;
+  color: white ;
+}
+</style>
